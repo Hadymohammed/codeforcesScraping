d17715a641e33606254936aaea9d31d0cc2809b5
Hadymohammed <Hadymohamed2002@gmail.com>
2023-04-25 03:29:13 +0200

    add setHandles & handls routes

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
 
-const Express=require("express");
-const axios=require("axios");
-const cheerio=require("cheerio");
+import express from "express";
+import axios from "axios";
+import cheerio from "cheerio";
+import CSSselect from "css-select";
+import getHandles from "./services/getHandles.js";
+const app=express();
+app.listen(8081,()=>{console.log("App is listening")});
+let handles=[];
+const contests=[{
+    id:381706,
+    problems:[50,20,30,40,50,30,50,60,70,20,20,30,45,70],
+    pages:1,
+}]
 
-const app=Express();
-app.listen(8080,()=>{console.log("App is listening")});
 
-const handles=['Abdelhady__'];//,'Zeyad_Abdelnaby'];
-const data=[];
-app.use(Express.json);
-for (const handle of handles) {
-    axios(`https://codeforces.com/profile/${handle}`)
-        .then(response =>{
-            const html=response.data;
-            const $=cheerio.load(html);
-            const total=$('._UserActivityFrame_counterValue')[0].children[0].data;
-            const maxInRow=$('._UserActivityFrame_counterValue')[3].children[0].data;
-            let cnt=0;
-            const from='07/03/2022',to='08/07/2022';
-            const days=$('rect[class=day]')._root[0].children;
-            console.log(days);
-            /*for (const day of days) {
-                if(day[0].children[0].att)
-            }*/
-           
-            
-            data.push({
-                name:handle,
-                totalSolved:total,
-                maxInRow:maxInRow
-            })
-        })
-        .catch(err=>{console.log(err)});
-}
-const router=Express.Router();
-
-router.get('/data',(req,res)=>{
-    for (const user of data) {
-        console.log(user);
-    }
+const router=express.Router();
+router.get('/setHandles', async (req,res)=>{
+  handles=await getHandles();
+  res.send('<h1>Handles have been setted<\h1>');
+})
+router.get('/handles',(req,res)=>{
+    res.send(handles);
 })
 app.use(router);
