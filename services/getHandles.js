2d57522b4fe2e3a2146f6c8b9f9e45dd4e7a8deb
Hadymohammed <Hadymohamed2002@gmail.com>
2023-04-27 03:10:28 +0200

    .env secret

--- services/getHandles.js
+++ services/getHandles.js
@@ -1,13 +1,15 @@
 import axios from "axios";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const apiKey='AIzaSyCVuRkg67PKH8_6TBtPWUOjHaDYftRuFXY';
+const {googleAPI_KEY}=process.env;
 const sheetId='1-x_csulzSnH3oegKh8WnEdtpsPbHkkVXJ8uuDL1to3Q';
 const range='E2:E58';
     
 const getHandles=async()=>{
     let handles=[];
     try{
-        const {data}=await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`)
+        const {data}=await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${googleAPI_KEY}`)
         for(let x of data.values)
                 handles.push(x[0]);
         return handles; 
